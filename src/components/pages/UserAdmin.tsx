32ebc800276be32e8876638cad095fb3377dd03c
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-12-07 11:10:11 +0900

    Userページ作成

--- src/components/pages/UserAdmin.tsx
+++ src/components/pages/UserAdmin.tsx
@@ -1,5 +1,85 @@
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC, useCallback } from "react";
+import {
+  Wrap,
+  WrapItem,
+  Spinner,
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  useDisclosure,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserAdmin: VFC = memo(() => {
-  return <p>ユーザー管理画面です</p>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, loading, users } = useAllUsers();
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  const onClickUser = useCallback(() => onOpen(), [onOpen]);
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users?.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay>
+          <ModalContent pb={6}>
+            <ModalHeader>ユーザー詳細</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody mx={4}>
+              <Stack spacing={4}>
+                <FormControl>
+                  <FormLabel>名前</FormLabel>
+                  <Input value="松本" isReadOnly />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>フルネーム</FormLabel>
+                  <Input value="Shogo Matsumoto" isReadOnly />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>メールアドレス</FormLabel>
+                  <Input value="12345@example.com" isReadOnly />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>電話番号</FormLabel>
+                  <Input value="090-1111-2222" isReadOnly />
+                </FormControl>
+              </Stack>
+            </ModalBody>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </>
+  );
 });
