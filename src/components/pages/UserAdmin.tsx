212cc96f685ca623739719d716e8ceaa89f71f51
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-12-08 11:05:26 +0900

    ログインユーザー情報をContextに保持

--- src/components/pages/UserAdmin.tsx
+++ src/components/pages/UserAdmin.tsx
@@ -10,11 +10,14 @@ import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserAdmin: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
   console.log(selectedUser);
   useEffect(() => {
     getUsers();
